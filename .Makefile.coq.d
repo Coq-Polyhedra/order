caab33f61738edca9b00846aa74fa1477cd09a81
Pierre-Yves Strub <pierre-yves@strub.nu>
2020-12-17 14:00:11 +0100

    remove build files

--- .Makefile.coq.d
+++
@@ -1,6 +0,0 @@
-src/xbigop.vo src/xbigop.glob src/xbigop.v.beautified src/xbigop.required_vo: src/xbigop.v 
-src/xbigop.vio: src/xbigop.v 
-src/xbigop.vos src/xbigop.vok src/xbigop.required_vos: src/xbigop.v 
-src/relorder.vo src/relorder.glob src/relorder.v.beautified src/relorder.required_vo: src/relorder.v src/xbigop.vo
-src/relorder.vio: src/relorder.v src/xbigop.vio
-src/relorder.vos src/relorder.vok src/relorder.required_vos: src/relorder.v src/xbigop.vos
